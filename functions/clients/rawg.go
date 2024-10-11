a285b45905ac2a74aadbbc0876dc6ad9a7cd68e6
ryanchiu2017 <pikachiurock@yahoo.com>
2024-10-10 23:30:07 -0700

    Add AutocompleteGames with rawg.go

--- functions/clients/rawg.go
+++ functions/clients/rawg.go
@@ -2,8 +2,10 @@ package clients
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/dimuska139/rawg-sdk-go/v3"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
@@ -35,4 +37,21 @@ func (r *Rawg) GetGame(ctx context.Context, game string) (*rawg.GameDetailed, er
 func (r *Rawg) SearchGame(ctx context.Context, name string, page, pageSize int) ([]*rawg.Game, int, error) {
 	ctxzap.Info(ctx, "RAWG: Searching games", zap.String("name", name), zap.String("type", "GetGames"))
 	return r.client.GetGames(ctx, rawg.NewGamesFilter().SetPageSize(pageSize).SetPage(page+1).SetStores(STEAM_STORE, GOG_STORE, EPIC_GAMES).SetSearch(name))
+}
+
+func (r *Rawg) AutocompleteGames(ctx context.Context, guildID, text string, entries int) ([]*discordgo.ApplicationCommandOptionChoice, error) {
+	gamesList, count, err := r.SearchGame(ctx, text, 1, entries)
+	if err != nil {
+		return []*discordgo.ApplicationCommandOptionChoice{}, fmt.Errorf("Rawg.SearchGame: %v", err)
+	}
+
+	results := make([]*discordgo.ApplicationCommandOptionChoice, 0, count)
+	for _, game := range gamesList {
+		results = append(results, &discordgo.ApplicationCommandOptionChoice{
+			Name:  game.Name,
+			Value: game.Slug,
+		})
+	}
+
+	return results, nil
 }
