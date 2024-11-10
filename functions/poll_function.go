c7186e4255266c9eee1b1cef6c5fc5a2f03d5358
PinkNoize <21967246+PinkNoize@users.noreply.github.com>
2024-11-10 12:07:08 -0800

    Add notification

--- functions/poll_function.go
+++ functions/poll_function.go
@@ -10,6 +10,7 @@ import (
 	"github.com/PinkNoize/flavor-of-the-week/functions/command"
 	"github.com/PinkNoize/flavor-of-the-week/functions/guild"
 	"github.com/PinkNoize/flavor-of-the-week/functions/setup"
+	"github.com/bwmarrin/discordgo"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
 )
@@ -23,14 +24,20 @@ func PollPubSub(ctx context.Context, _ PubSubMessage) error {
 	}()
 	ctx = ctxzap.ToContext(ctx, logger)
 
+	now := time.Now().UTC()
+
 	ctxzap.Info(ctx, "Starting poll job")
 	err = endActivePolls(ctx, setup.ClientLoader)
 	if err != nil {
 		slogger.Errorf("endActivePolls: %v", err)
 	}
-	err = startScheduledPolls(ctx, setup.ClientLoader)
+	err = startScheduledPolls(ctx, now, setup.ClientLoader)
 	if err != nil {
 		slogger.Errorf("startScheduledPolls: %v", err)
+	}
+	err = notifyUpcomingPolls(ctx, now, setup.ClientLoader)
+	if err != nil {
+		slogger.Errorf("notifyUpcomingPolls: %v", err)
 	}
 	return nil
 }
@@ -80,8 +87,7 @@ func endActivePolls(ctx context.Context, cl *clients.Clients) error {
 	return nil
 }
 
-func startScheduledPolls(ctx context.Context, cl *clients.Clients) error {
-	now := time.Now().UTC()
+func startScheduledPolls(ctx context.Context, now time.Time, cl *clients.Clients) error {
 	day := now.Weekday()
 	hour := now.Hour()
 	ctxzap.Info(ctx, fmt.Sprintf("Searching for schedules with Day %v, Hour %v", day, hour))
@@ -100,6 +106,49 @@ func startScheduledPolls(ctx context.Context, cl *clients.Clients) error {
 		_, err = cmd.Execute(ctx, cl)
 		if err != nil {
 			ctxzap.Warn(ctx, fmt.Sprintf("StartPollCommand: %v", err))
+			continue
+		}
+	}
+	return nil
+}
+
+func notifyUpcomingPolls(ctx context.Context, now time.Time, cl *clients.Clients) error {
+	discordSession, err := cl.Discord()
+	if err != nil {
+		return fmt.Errorf("Discord: %v", err)
+	}
+
+	tomorrow := now.Add(24 * time.Hour)
+	day := tomorrow.Weekday()
+	hour := tomorrow.Hour()
+	ctxzap.Info(ctx, fmt.Sprintf("Searching for schedules with Day %v, Hour %v for notif", day, hour))
+	guilds, err := guild.GetGuildsWithSchedule(ctx, day, hour, cl)
+	if err != nil {
+		return fmt.Errorf("GetGuildsWithSchedule: %v", err)
+	}
+
+	ctxzap.Info(ctx, fmt.Sprintf("Found %v scheduled polls", len(guilds)))
+	prevContext := ctx
+	for _, g := range guilds {
+		ctx = prevContext
+		ctxzap.AddFields(ctx, zap.String("guildID", g.GetGuildId()))
+
+		pollChannel, err := g.GetPollChannel(ctx)
+		if err != nil {
+			ctxzap.Warn(ctx, fmt.Sprintf("GetPollChannel: %v", err))
+			continue
+		}
+		_, err = discordSession.ChannelMessageSendComplex(*pollChannel, &discordgo.MessageSend{
+			Embeds: []*discordgo.MessageEmbed{
+				{
+					Title:       "⏳24 hours until the next poll⌛",
+					Description: "Get your nominations in before it's too late.\nType */nominations* to get started.",
+					Color:       2326507,
+				},
+			},
+		})
+		if err != nil {
+			ctxzap.Warn(ctx, fmt.Sprintf("ChannelMessageSendComplex: %v", err))
 			continue
 		}
 	}
