646ed5024bc5fada3e0e5135c9c26cbfc4b1841b
PinkNoize <21967246+PinkNoize@users.noreply.github.com>
2024-11-12 21:10:47 -0800

    Add banned users feature

--- functions/clients/clients.go
+++ functions/clients/clients.go
@@ -2,11 +2,17 @@ package clients
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"os"
 
 	"cloud.google.com/go/firestore"
+	"cloud.google.com/go/storage"
 	"github.com/bwmarrin/discordgo"
 	"github.com/josestg/lazy"
+	"google.golang.org/api/googleapi"
 )
 
 type Clients struct {
@@ -15,6 +21,7 @@ type Clients struct {
 	firestoreClient *lazy.Loader[*firestore.Client]
 	discordSession  *lazy.Loader[*discordgo.Session]
 	rawgClient      *lazy.Loader[*Rawg]
+	bannedUsers     *lazy.Loader[map[string]struct{}]
 }
 
 func New(ctx context.Context, projectID, discordToken, rawgToken string) *Clients {
@@ -35,10 +42,43 @@ func New(ctx context.Context, projectID, discordToken, rawgToken string) *Client
 	r := lazy.New(func() (*Rawg, error) {
 		return NewRawg(rawgToken), nil
 	})
+	bU := lazy.New(func() (map[string]struct{}, error) {
+		client, err := storage.NewClient(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create storage client: %v", err)
+		}
+		rc, err := client.Bucket(os.Getenv("RESOURCES_BUCKET")).Object("banned-users.json").NewReader(ctx)
+		if err != nil {
+			// return empty list if doesn't exist
+			var e *googleapi.Error
+			if ok := errors.As(err, &e); ok {
+				if e.Code == 404 {
+					return nil, nil
+				}
+			}
+			return nil, fmt.Errorf("failed to read object: %v", err)
+		}
+		defer rc.Close()
+		body, err := io.ReadAll(rc)
+		if err != nil {
+			return nil, fmt.Errorf("readAll: %v", err)
+		}
+		var userList []string
+		err = json.Unmarshal(body, &userList)
+		if err != nil {
+			return nil, fmt.Errorf("Unmarshal: %v", err)
+		}
+		userLookup := make(map[string]struct{})
+		for _, user := range userList {
+			userLookup[user] = struct{}{}
+		}
+		return userLookup, nil
+	})
 	return &Clients{
 		firestoreClient: &f,
 		discordSession:  &d,
 		rawgClient:      &r,
+		bannedUsers:     &bU,
 	}
 }
 
@@ -56,4 +96,8 @@ func (c *Clients) Discord() (*discordgo.Session, error) {
 
 func (c *Clients) Rawg() *Rawg {
 	return c.rawgClient.Value()
+}
+
+func (c *Clients) BannedUsers() (map[string]struct{}, error) {
+	return c.bannedUsers.Value(), c.bannedUsers.Error()
 }
